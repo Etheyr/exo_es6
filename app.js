32f66e54d620e01316f81c8ac5f5e769ef1963b8
axel <axel.bouillart@gmail.com>
2016-12-20 11:12:32 +0100

    update

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var nasusStack = function nasusStack() {
+var tab = [];
+
+document.getElementById('envoi').addEventListener('click', function () {
 	var btn = document.getElementById('inputName').value;
-	var listNasus = document.getElementById('listInput').innerHTML = '<li>' + btn + '</li>';
+	var listNasus = document.body.appendChild(document.createElement('li')).innerHTML = btn;
 	console.log(btn);
 	if (btn === '') {
 		var nasusSansStack = document.getElementById('nasusSpawn').innerHTML = 'Nasus est à la fête de noël';
 	} else {
 		var spawnTxt = document.getElementById('nasusSpawn').innerHTML = btn + ' est soyeux et raffiné';
 	}
-};
+});
--- begin.es6.js
+++ begin.es6.js
@@ -1,11 +1,12 @@
+	let tab =[];
 
-let nasusStack = ()=> {
-	let btn = document.getElementById('inputName').value;
-	let listNasus = document.getElementById('listInput').innerHTML =  '<li>' + btn + '</li>';
-	console.log(btn);
-	if (btn === '') {
-		let nasusSansStack = document.getElementById('nasusSpawn').innerHTML = 'Nasus est à la fête de noël';
-	} else {
-		let spawnTxt = document.getElementById('nasusSpawn').innerHTML = btn + ' est soyeux et raffiné'; 
-	}
-}
+	document.getElementById('envoi').addEventListener('click' ,()=> {
+		let btn = document.getElementById('inputName').value;
+		let listNasus = document.body.appendChild(document.createElement('li')).innerHTML = btn ;
+		console.log(btn);
+		if (btn === '') {
+			let nasusSansStack = document.getElementById('nasusSpawn').innerHTML = 'Nasus est à la fête de noël';
+		} else {
+			let spawnTxt = document.getElementById('nasusSpawn').innerHTML = btn + ' est soyeux et raffiné'; 
+		}
+	});

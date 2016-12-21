e8991c58766c807805d0bc3ca3e14500f21a302e
axel <axel.bouillart@gmail.com>
2016-12-21 16:36:18 +0100

    finish

--- app.js
+++ app.js
@@ -1,14 +1,62 @@
 'use strict';
 
-var tab = [];
+(function () {
 
-document.getElementById('envoi').addEventListener('click', function () {
-	var btn = document.getElementById('inputName').value;
-	var listNasus = document.body.appendChild(document.createElement('li')).innerHTML = btn;
-	console.log(btn);
-	if (btn === '') {
-		var nasusSansStack = document.getElementById('nasusSpawn').innerHTML = 'Nasus est à la fête de noël';
-	} else {
-		var spawnTxt = document.getElementById('nasusSpawn').innerHTML = btn + ' est soyeux et raffiné';
-	}
-});
+	var app = {
+
+		tabName: [],
+		tabFilter: [],
+
+		init: function init() {
+
+			this.DisplayTXT();
+			this.SearchFilter();
+
+		},
+		
+		DisplayTXT: function DisplayTXT() {
+			var _this = this;
+
+			document.getElementById('btnName').addEventListener('click', function () {
+
+				var btn = document.getElementById('name').value;
+				console.log(btn);
+				var majName = btn.charAt(0).toUpperCase();
+				console.log(majName);
+				var Name = majName + btn.slice(1);
+
+				if (btn === '') {
+					var nasusSansStack = document.getElementById('DisplayTxt').innerHTML = 'Nasus est à la fête de noël';
+				} else {
+					var listNasus = document.body.appendChild(document.createElement('li')).innerHTML = Name;
+					var spawnTxt = document.getElementById('DisplayTxt').innerHTML = btn + ' est soyeux et raffin\xE9';
+					_this.tabName.push(btn);
+					console.log(_this.tabName);
+				}
+			});
+		},
+
+		SearchFilter: function SearchFilter() {
+			var _this2 = this;
+
+			document.getElementById('btnSearch').addEventListener('click', function () {
+
+				var btnSearch = document.getElementById('search').value;
+				var minSearch = btnSearch.toLowerCase();
+
+				for (var i = 0; i < _this2.tabName.length; i++) {
+					var element = _this2.tabName[i];
+					console.log(element);
+					var filter = element.indexOf(minSearch);
+					console.log(filter);
+
+					if (filter === 0) {
+						_this2.tabFilter.push(element);
+						document.getElementById('result').innerHTML = _this2.tabFilter;
+					}
+				}
+			});
+		}
+	};
+	app.init();
+})();
--- begin.es6.js
+++ begin.es6.js
@@ -1,12 +1,76 @@
-	let tab =[];
+	(()=>{
 
-	document.getElementById('envoi').addEventListener('click' ,()=> {
-		let btn = document.getElementById('inputName').value;
-		let listNasus = document.body.appendChild(document.createElement('li')).innerHTML = btn ;
-		console.log(btn);
-		if (btn === '') {
-			let nasusSansStack = document.getElementById('nasusSpawn').innerHTML = 'Nasus est à la fête de noël';
-		} else {
-			let spawnTxt = document.getElementById('nasusSpawn').innerHTML = btn + ' est soyeux et raffiné'; 
+		const app = {
+
+			tabName: [],
+			tabFilter: [],
+
+
+			init(){
+
+				this.DisplayTXT();
+				this.SearchFilter();
+
+			},
+
+			DisplayTXT(){
+
+				document.getElementById('btnName').addEventListener('click' ,()=> {
+
+					let btn = document.getElementById('name').value;
+					console.log(btn);
+					let majName = btn.charAt(0).toUpperCase();
+					console.log(majName)
+					let Name = majName + btn.slice(1);
+
+					if (btn === '') {
+						let nasusSansStack = document.getElementById('DisplayTxt').innerHTML = 'Nasus est à la fête de noël';
+					} else {
+						let listNasus = document.body.appendChild(document.createElement('li')).innerHTML = Name ;
+						let spawnTxt = document.getElementById('DisplayTxt').innerHTML = `${btn} est soyeux et raffiné`; 
+						this.tabName.push(btn);
+						console.log(this.tabName);
+					}
+
+				});
+			},
+			SearchFilter(){
+
+				document.getElementById('btnSearch').addEventListener('click' ,()=> {
+
+					let btnSearch = document.getElementById('search').value;
+					let minSearch = btnSearch.toLowerCase();
+
+					for(const i= 0; i< this.tabName.length; i++){
+						let element = this.tabName[i];
+						console.log(element);
+						let filter = element.indexOf(minSearch);
+						console.log(filter);
+
+						if(filter === 0){
+							this.tabFilter.push(element);
+							document.getElementById('result').innerHTML = this.tabFilter;
+						}
+					}
+				});
+			}
 		}
-	});
+		app.init();
+	})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
